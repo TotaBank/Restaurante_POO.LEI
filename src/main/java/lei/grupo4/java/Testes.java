aa51a0b1bf9c17ac75d4e569977b03ecd95956d3
prxL22 <jfpego2005@gmail.com>
2025-12-30 16:05:57 +0000

    MenuItem.java podeSerPreparado() e consumirStock() Stock.java

--- src/main/java/lei/grupo4/java/Testes.java
+++ src/main/java/lei/grupo4/java/Testes.java
@@ -9,5 +9,33 @@ public class Testes {
         Reserva.criarReserva(LocalDate.of(2025,12,31), Refeicao.JANTAR, 3 , "Tomas");
         //System.out.println(Mesa.obterPorCapacidade(8));
 
+        /* //teste pode ser preparado e consume itens do stock
+        // Cria o MenuItem
+        MenuItem hamburger = new MenuItem(1, "Hambúrguer Clássico", 8.5f, true, null);
+
+        // Verifica se pode ser preparado
+        if (hamburger.podeSerPreparado()) {
+            System.out.println("Hambúrguer pode ser preparado");
+
+            // Remove stock
+            hamburger.consumirStock();
+            System.out.println("Stock atualizado após pedido");
+        } else {
+            System.out.println("Hambúrguer NÃO pode ser preparado");
+        }
+        */
+
+
+        /* //teste restock
+        Stock.repor(1, 10); // repõe 10 unidades de Carne
+        Stock.repor(4, 5);  // repõe 5 unidades de Alface
+        */
+
+        /* //teste de stock minimo
+        Stock.abaixoDoMinimo();
+        */
+
+
     }
 }
+
